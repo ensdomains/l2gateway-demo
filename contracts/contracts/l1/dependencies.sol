8dfbcbc224a8ff520a12acb4c60e5c1a74c20f95
Nick Johnson <arachnid@notdot.net>
2021-03-22 14:40:00 +1300

    Move contracts into a single hardhat project

--- contracts/contracts/l1/dependencies.sol
+++ contracts/contracts/l1/dependencies.sol
@@ -1,2 +1,4 @@
+// @unsupported: ovm
 import { OVM_CanonicalTransactionChain } from "@eth-optimism/contracts/build/contracts/OVM/chain/OVM_CanonicalTransactionChain.sol";
+// @unsupported: ovm
 import { OVM_StateCommitmentChain } from "@eth-optimism/contracts/build/contracts/OVM/chain/OVM_StateCommitmentChain.sol";
